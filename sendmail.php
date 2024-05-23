5a87c0a83624099c08248b495bea314b3c38187d
kreks211 <dimitriy.bogatiy@yandex.ru>
2024-05-23 22:41:17 +0400

    Create sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,30 @@
+&lt;?php
+require 'PHPMailerAutoload.php';
+ 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+ 
+$mail = new PHPMailer;
+$mail-&gt;isSMTP();
+$mail-&gt;Host = 'smtp.yandex.ru';
+$mail-&gt;SMTPAuth = true;
+$mail-&gt;Username = 'dimitriy.bogatiy@yandex.ru';
+$mail-&gt;Password = '199906i2dB';
+$mail-&gt;SMTPSecure = 'tls';
+$mail-&gt;Port = 587;
+ 
+$mail-&gt;setFrom($email, $name);
+$mail-&gt;addAddress('dimitriy.bogatiy@yandex.ru');
+$mail-&gt;isHTML(true);
+ 
+$mail-&gt;Subject = 'Сообщение с формы обратной связи';
+$mail-&gt;Body    = "От: $name &lt;br&gt; Email: $email &lt;br&gt; Сообщение: $message";
+ 
+if(!$mail-&gt;send()) {
+    echo 'Ошибка при отправке сообщения.';
+    echo 'Mailer Error: ' . $mail-&gt;ErrorInfo;
+} else {
+    echo 'Сообщение успешно отправлено!';
+}
+?&gt;
